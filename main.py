986691f2352b28472d3b49363da87663744f77fc
Alex Shortt <Alex Shortt>
2017-11-15 22:28:44 -0800

    loop through x iterations, print out all info

--- main.py
+++ main.py
@@ -4,50 +4,53 @@ import soundcloud
 import requests
 from bs4 import BeautifulSoup
 
-#quote_page = 'https://soundcloud.com/regardscoupables'
-#page = urllib.urlopen(quote_page)
-#soup = BeautifulSoup(page, 'html.parser')
-#print(soup)
-#name_box = soup.find('div', attrs={'class' : 'truncatedUserDescription__content'})
-#name = name_box.text.strip() #strip() removes starting and trailing tags
-#print(name)
-
 client_id = "a3e059563d7fd3372b49b37f00a00bcf"
-#client = soundcloud.Client(client_id)
+iterations = 5
 
 class SoundCloudUser:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, username=None, id=None):
+        if id != None:
+            self.id = id
+        else:
+            self.username = username
+            api = ("http://api.soundcloud.com/resolve?url=https://soundcloud.com/" + str(self.username) + "&client_id=" + client_id)
+            r = requests.get(api)
+            try:
+                self.id = r.json()['id']
+            except:
+                print("Invalid Initial Username")
         
     def followerList(self):
         api = ("http://api.soundcloud.com/users/" + str(self.id) + "/followings?client_id=" + client_id)
         r =  requests.get(api)
         try:
             collection = r.json()['collection']
-            for val in collection:
-                print(val['id'])
+            return collection
         except:
             print("Invalid Response")
             
-    def getData(self):
+    def printData(self):
         api = ("http://api.soundcloud.com/users/" + str(self.id) + "?client_id=" + client_id)
         r = requests.get(api)    
         try:
-            username = r.json()['username']
-            userId = r.json()['id']
-            desc = r.json()['description']
-            follow_count = r.json()['followers_count']
-            url = "https://soundcloud.com/" + str(r.json()['permalink'])
+            username = str(r.json()['username'])
+            userId = str(r.json()['id'])
+            desc = str(r.json()['description']).replace('\n', " ")
+            follow_count = str(r.json()['followers_count'])
+            url = str("https://soundcloud.com/" + str(r.json()['permalink']))
             
-            print("Username: " + str(username))
-            print("User ID: " + str(userId))
-            print("Description: " + str(desc))
-            print("Followers: " + str(follow_count))
-            print("Url: " + str(url))
+            print(username + " :: " + userId + " :: " + desc + " :: " + follow_count + " :: " + url)
         except:
             print("Invalid Response")
             
 
 #main
-user = SoundCloudUser(1433)
-user.followerList()
+user = SoundCloudUser("alex_shortt", None)
+list = user.followerList()
+
+for x in range(0, iterations):
+    for val in list:
+        follow_user = SoundCloudUser(None, val['id'])
+        follow_user.printData()
+    user = SoundCloudUser(None, list[0]['id'])
+    list = user.followerList()
